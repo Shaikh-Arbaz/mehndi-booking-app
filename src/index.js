3c95d6dc5ba9a45770d7d591953a96186f3b86a7
Arbaz Shaikh <arbaz@example.com>
2025-08-01 18:22:46 +0530

    New Code Changes

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css'; // if you created one
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
